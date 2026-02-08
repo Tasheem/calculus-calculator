4f5105d86f6df97ca80dd7cd56448e1067259566
Tasheem <thargro1@uab.edu>
2026-02-08 14:39:04 -0600

    Add the rest of the polynomial logic

--- algebra/models/polynomial.py
+++ algebra/models/polynomial.py
@@ -6,17 +6,26 @@ class Polynomial(Expression):
         super().__init__()
 
         if isinstance(expression_or_coefficients, (Sum, Difference)):
-            self.coefficients = []
-            self._construct_polynomial(expression_or_coefficients)
+            self.variable: Variable | None = None
+            self.coefficients: list[Constant] = []
+            self._construct_polynomial(expression_or_coefficients, None)
         else:
-            self.variable = expression_or_coefficients[0]
-            self.coefficients = expression_or_coefficients[1]
+            self.variable = Variable(expression_or_coefficients[0])
+            self.coefficients = [Constant(n) for n in expression_or_coefficients[1]]
 
-    def _construct_polynomial(self, expression: Expression | None):
+    def degree(self):
+        return len(self.coefficients) - 1
+
+    def _construct_polynomial(self, expression: Expression | None, parent: Expression | None):
+        distribute_minus = isinstance(parent, Difference) and expression is parent.right_side
+
         if expression is None:
             return
-        elif isinstance(expression, Constant): # 0th degree
-            self._add_coefficient(int(expression.value), 0)
+        elif isinstance(expression, Constant): # 0-degree
+            self._add_coefficient(int(expression.value), None, 0, distribute_minus)
+        elif isinstance(expression, Variable):
+            # i.e. The x in the equation 2x² + x + 8
+            self._add_coefficient(1, expression, 1, False)
         elif isinstance(expression, Power):
             base = expression.base
             exponent = expression.exponent
@@ -24,15 +33,59 @@ class Polynomial(Expression):
                 raise ValueError("A power node cannot contain a non-Constant type as its exponent. This is an invalid polynomial.")
 
             if isinstance(base, Variable): # i.e x²
-                self._add_coefficient(1, int(exponent.value))
-            elif isinstance(base, Product):
+                self._add_coefficient(1, base, int(exponent.value), distribute_minus)
+            elif isinstance(base, Product): # i.e (2x)²
                 if isinstance(base.left_side, Constant) and isinstance(base.right_side, Variable):
-                    self._add_coefficient(int(base.left_side.value), int(exponent.value))
+                    self._add_coefficient(int(base.left_side.value), base.right_side, int(exponent.value), distribute_minus)
                 else:
                     raise ValueError("The base of this Power is a Product type. Expected the Product to contain a Constant on its left and Variable on its right.")
+            else:
+                raise ValueError("This Power node contains a base which should not exist in this polynomial:", expression, base)
+        elif isinstance(expression, Product): # i.e 2x -> Product(Constant(2), Variable("x")) or 2x² -> Product(Constant(2), Power(Variable("x"), Constant(2)))
+            if not isinstance(expression.left_side, Constant):
+                raise ValueError("A Product node cannot contain a non-Constant type on its left side. This is an invalid polynomial.")
+            
+            if isinstance(expression.right_side, Variable): # 1-degree
+                self._add_coefficient(int(expression.left_side.value), expression.right_side, 1, distribute_minus)
+            elif isinstance(expression.right_side, Power): # nth-degree
+                base = expression.right_side.left_side
+                exponent = expression.right_side.right_side
+                if not isinstance(exponent, Constant):
+                    raise ValueError("A power node cannot contain a non-Constant type as its exponent. This is an invalid polynomial.")
 
-    def _add_coefficient(self, coefficient: int, degree: int):
+                if not isinstance(base, Variable): # i.e x²
+                    raise ValueError("A product node cannot contain a power node with a non-Variable type as its base.")
+                
+                self._add_coefficient(int(expression.left_side.value), base, int(exponent.value), distribute_minus)
+            else:
+                raise ValueError("This Product node contains a node on its right side which should not exist in this polynomial:", expression, expression.right_side)
+        elif isinstance(expression, (Sum, Difference)):
+            # Recursive case.
+            # Recursive right first
+            self._construct_polynomial(expression.right_side, expression)
+            self._construct_polynomial(expression.left_side, expression)
+        else:
+            raise ValueError("This expression contains a node which should not exist in this polynomial:", expression)
+
+    def _add_coefficient(self, coefficient: int, variable: Variable | None, degree: int, distribute_minus: bool):
         while len(self.coefficients) <= degree:
-            self.coefficients.append(0)
+            self.coefficients.append(Constant(0))
 
-        self.coefficients[degree] = coefficient
+        # If there is already a non-zero value at this degree, then we have duplicate degrees
+        if self.coefficients[degree].value != 0:
+            raise ValueError(f"Duplicates at degree: {degree}", self.coefficients[degree].value, coefficient)
+        
+        # Set the variable equal to the variable at this degree.
+        if variable is not None:
+            self._set_variable(variable)
+
+        self.coefficients[degree] = Constant(coefficient * -1) if distribute_minus else Constant(coefficient)
+
+    def _set_variable(self, exp_var: Variable):
+        if self.variable is None:
+            # Assume the first variable we come across is the single variable for this polynomial
+            self.variable = exp_var.copy()
+        elif self.variable.name != exp_var.name:
+            # Raise error if we come across a second variable
+            # This program will not support multivariable polynomials
+            raise ValueError("Multivariable polynomials are not supported.", self.variable.name, exp_var.name)
--- algebra/tests/test_polynomial.py
+++ algebra/tests/test_polynomial.py
@@ -1,1 +1,88 @@
 from algebra.models.polynomial import *
+
+# x + 5
+def test_construct_from_expression_1():
+    x = Variable("x")
+    five = Constant(5)
+    equation = Sum(x, five)
+    p = Polynomial(equation)
+    
+    assert p.variable is not None and p.variable.name == "x"
+    assert p.degree() == 1
+    assert p.coefficients[1].value == 1
+    assert p.coefficients[0].value == 5
+
+# 3x - 2
+def test_construct_from_expression_2():
+    product = Product(Constant(3), Variable("x"))
+    two = Constant(2)
+    equation = Difference(product, two)
+    p = Polynomial(equation)
+
+    assert p.variable is not None and p.variable.name == "x"
+    assert p.degree() == 1
+    assert p.coefficients[1].value == 3
+    assert p.coefficients[0].value == -2
+
+# t² - 4t + 2
+def test_construct_from_expression_3():
+    two_deg = Power(Variable("t"), Constant(2))
+    one_deg = Product(Constant(4), Variable("t"))
+    zero_deg = Constant(2)
+
+    equation = Sum(Difference(two_deg, one_deg), zero_deg)
+    p = Polynomial(equation)
+
+    assert p.variable is not None and p.variable.name == "t"
+    assert p.degree() == 2
+    assert p.coefficients[2].value == 1
+    assert p.coefficients[1].value == -4
+    assert p.coefficients[0].value == 2
+
+# 5t³ + 2t² - 16t - 8
+def test_construct_from_expression_4():
+    three_deg = Product(Constant(5), Power(Variable("t"), Constant(3)))
+    two_deg = Product(Constant(2), Power(Variable("t"), Constant(2)))
+    one_deg = Product(Constant(16), Variable("t"))
+    zero_deg = Constant(8)
+    
+    # Evalutate from left to right since add and subtract take equal precendence in PEMDAS.
+    add_three_two = Sum(three_deg, two_deg)
+    sub_res_one = Difference(add_three_two, one_deg)
+    sub_res_zero = Difference(sub_res_one, zero_deg)
+    p = Polynomial(sub_res_zero)
+
+    assert p.variable is not None and p.variable.name == "t"
+    assert p.degree() == 3
+    assert p.coefficients[3].value == 5
+    assert p.coefficients[2].value == 2
+    assert p.coefficients[1].value == -16
+    assert p.coefficients[0].value == -8
+
+# 2x² - 1
+def test_construct_from_expression_5():
+    two_deg = Product(Constant(2), Power(Variable("x"), Constant(2)))
+    zero_deg = Constant(1)
+
+    equation = Difference(two_deg, zero_deg)
+    p = Polynomial(equation)
+
+    assert p.variable is not None and p.variable.name == "x"
+    assert p.degree() == 2
+    assert p.coefficients[2].value == 2
+    assert p.coefficients[1].value == 0
+    assert p.coefficients[0].value == -1
+
+# x² * 2x + 1
+def test_construct_from_expression_6():
+    two_deg = Power(Variable("x"), Constant(2))
+    one_deg = Product(Constant(2), Variable("x"))
+    zero_deg = Constant(1)
+
+    equation = Sum(Product(two_deg, one_deg), zero_deg)
+    
+    try:
+        p = Polynomial(equation)
+        assert False
+    except:
+        assert True
